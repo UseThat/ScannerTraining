eb030406df82b6be25e942e34771eec81f276c95
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-02-07 08:14:50 +0100

    Add full logic with test.

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,7 +1,32 @@
 public class Calculate {
-    public double calculate (double a, double b, String operation){
-        return 0;
-    }
+    private final String add = "+";
+    private final String subtract = "-";
+    private final String divide = "/";
+    private final String multiply = "*";
 
-    
+
+    public double calculate(double a, double b, String operation){
+        switch (operation){
+            case add:
+                return a+b;
+
+            case subtract:
+                return a-b;
+
+            case divide:
+                if(b == 0){
+                    System.out.println("Unable to divide by 0");
+                    return 0;
+                }else{
+                    return a/b;
+                }
+
+            case multiply:
+                return a*b;
+
+            default:
+                System.out.println("No option choose");
+                return 0;
+        }
+    }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,23 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Calculate calculate = new Calculate();
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.print("Set A: ");
+        double a = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Operation: ");
+        String operation = scanner.nextLine();
+        System.out.print("Set B: ");
+        double b =scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Result: "+calculate.calculate(a,b,operation));
+
+    }
 }
